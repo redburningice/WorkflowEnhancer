861927610b6fe0ad643327677f99f3337b2c16cc
Fabian Schmidberger <fabi1998@live.de>
2024-09-08 23:18:21 +0200

    Initial Commit

---
+++ src/old/Point.java
@@ -0,0 +1,24 @@
+package old;
+
+public class Point {
+    int xCoordinate;
+    int yCoordinate;
+
+    public Point(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getxCoordinate() + "," + getyCoordinate() + ")";
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+}
---
+++ src/old/TestStuff.java
@@ -0,0 +1,11 @@
+package old;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestStuff {
+    public static void main(String[] args) {
+        System.out.println("\u2610\u2612");
+    }
+}
+
